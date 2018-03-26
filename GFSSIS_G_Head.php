0b2629ee891db29bb6fa40936a2b9599e1a20d26
xxxjulsxxx <37772092+xxxjulsxxx@users.noreply.github.com>
2018-03-26 10:15:24 -0700

    Create GFSSIS_G_Head.php

---
+++ GFSSIS_G_Head.php
@@ -0,0 +1,21 @@
+<?php?>
+<head>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="ThemeBucket">
+    <link rel="shortcut icon" href="images/favicon.png">
+
+    <!--Core CSS -->
+    <link href="bs3/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/jquery.steps.css?1">
+
+    <!-- Custom styles for this template -->
+    <link href="css/custom.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+</head>
+
