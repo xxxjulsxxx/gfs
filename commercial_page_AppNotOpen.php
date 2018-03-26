7e5a3ea82979c2f1e57fd5adee9de3238c9c0cc0
xxxjulsxxx <37772092+xxxjulsxxx@users.noreply.github.com>
2018-03-26 10:12:48 -0700

    Update commercial_page_AppNotOpen.php

--- commercial_page_AppNotOpen.php
+++ commercial_page_AppNotOpen.php
@@ -1,1 +1,25 @@
-ssss
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'GFSSIS_G_Head.php'; ?>
+    <title>GFS | Application</title>
+</head>
+<body>
+    <section id="main-content">
+      <section class="wrapper"> 
+        <div class="row">
+          <div class="app-form"> 
+            <div>
+              <h1 class="app-title"><i class="fa fa-calendar"> Application not yet Open...</i></h1>
+              <p class="app-details"> Sorry for the inconvenience but the schedule for online application of <br> Grace Flous School is not yet open.
+              We will be posting the schedule <br> on our school page for details.<br><br>
+              Thank you!</p>
+            </div>
+            <div class="form-divider"></div>
+            <a href="#" class="back-btn"><i class="fa fa-home"></i> Back to Home</a>
+          </div>
+        </div>
+      </section>
+    </section>
+</body>
+</html>
