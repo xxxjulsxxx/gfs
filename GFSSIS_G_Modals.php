134be327484731a749bebcc360fe87a30cf7508d
xxxjulsxxx <37772092+xxxjulsxxx@users.noreply.github.com>
2018-03-26 10:17:07 -0700

    Create GFSSIS_G_Modals.php

---
+++ GFSSIS_G_Modals.php
@@ -0,0 +1,20 @@
+<!-- Forgot Password Modal start-->
+<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="forgotPassModal" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header" style="background: #405d27">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true" style="color: white">&times;</button>
+        <h4 class="modal-title">Forgot Password</h4>
+      </div>
+      <div class="modal-body">
+        <p>Enter your e-mail address below to reset your password.</p>
+        <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
+      </div>
+      <div class="modal-footer">
+        <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
+        <button class="btn btn-success" type="button">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- forgot passowrd modal end -->
