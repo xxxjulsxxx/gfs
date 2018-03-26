63a3a5ceccfba7dfcc1ba6eb1fc8a4eef39d0022
xxxjulsxxx <37772092+xxxjulsxxx@users.noreply.github.com>
2018-03-26 10:16:27 -0700

    Create GFSSIS_G_Header.php

---
+++ GFSSIS_G_Header.php
@@ -0,0 +1,32 @@
+
+<!--header start-->
+<header class="header fixed-top clearfix">
+<!--logo start-->
+<div class="brand">
+    <img class="logo" src="images/gfs6.png" alt="">
+</div>
+<!--logo end-->
+<div class="top-nav clearfix">
+    <ul class="nav pull-right top-menu">
+    <!-- user login dropdown start-->
+        <li class="dropdown">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                <img alt="" src="images/avatar1_small.jpg">
+                <!--<span class="username"><?php echo $fname," ", $mname, " ", $lname ; ?></span>-->
+                <b class="caret"></b>
+            </a>
+            <ul class="dropdown-menu extended logout">
+                <li>
+                    <a href="#changePassModal" data-toggle="modal"><i class="fa fa-key"></i> Change Password</a>
+                </li>
+                <li>
+                    <a href="#logoutModal" data-toggle="modal"><i class="fa fa-sign-out"></i> Log Out</a>
+                </li>
+            </ul>  
+        </li>
+    <!-- user login dropdown end -->
+    </ul>
+</div>
+</header>
+
+
